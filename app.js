b58a6fd6cd04ca3941049138ef5eab0c3bf35709
ragielly <ragielly.19@gmail.com>
2025-10-01 13:26:38 -0300

    adicionando função

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do numero Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero de 1 a 10';
+
+function verificarChute(){
+    console.log('O botão foi clicado');
+}
